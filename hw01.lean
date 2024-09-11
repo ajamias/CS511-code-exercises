f118df2f40faf19105236b8600c3bb8192d1361d
ajamias <ajamias@bu.edu>
2024-09-11 15:23:33 -0400

    update hw01 code

--- hw01.lean
+++ hw01.lean
@@ -1,24 +1,27 @@
 import Mathlib.Data.Real.Basic
 import Library.Basic
 
+math2001_init
 
-/- Exercise 3 | Example 1.3.4 in Macbeth -/
+/- # CS 511 Homework 1 -/
+
+/- Exercise 3: Example 1.3.4 in Macbeth -/
 example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by
   calc
     w = ((3 * w + 1) - 1)/3 := by ring
     _ = ((4) - 1)/3 := by rw [h1]
     _ = 1 := by ring
 
-/- Exercise 4 | Example 1.3.9 in Macbeth-/
+/- Exercise 4: Example 1.3.9 in Macbeth-/
 example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 := by
   calc
     a ^ 2 - a + 3 = (a - 3) ^ 2 + 5 * (a - 3) + 9 := by ring
     _ = (2 * b) ^ 2 + 5 * (2 * b) + 9 := by rw [h1]
     _ = 4 * b ^ 2 + 10 * b + 9 := by ring
 
-/- Problem 2 | Exercise 7 Section 1.3.11 in Macbeth -/
+/- Problem 2: Exercise 7 Section 1.3.11 in Macbeth -/
 example {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3)
-    (h3 : c = 1) : a = 2 :=
+    (h3 : c = 1) : a = 2 := by
   calc
     a = a + 2 * b + 3 * c - (2 * b + 3 * c) := by ring
     _ = 7 - (2 * b + 3 * c) := by rw [h1]
